9c622efe9509e6c702c1d3946cc4027c36b3594c
the-duck <markspurgeon96@hotmail.com>
2018-07-11 12:23:31 +0200

    code base, didn't test binary

---
+++ src/skriptoplugin.js
@@ -0,0 +1,122 @@
+#!/usr/bin/env node
+'use strict';
+
+var program = require('commander');
+var babel = require("babel-core");
+var fs = require("fs");
+var path = require("path");
+var process = require("process");
+var browserify = require('browserify');
+
+var buildAction = (src, output) => {
+  console.log(src, output);
+  console.log('Building plugin');
+  var sourceRel = src;
+  var destRel = output;
+  console.log(sourceRel, destRel);
+  var tempRel = 'temp/';
+
+  if (path.isAbsolute(sourceRel)) {
+    var sourcePath = sourceRel
+  } else {
+    sourcePath = path.join(process.cwd(), sourceRel);
+  }
+  if (path.isAbsolute(sourceRel)) {
+    var destPath = destRel
+  } else {
+    destPath = path.join(process.cwd(), destRel);
+  }
+  var tempPath = path.join(process.cwd(), tempRel);
+
+
+  var babelOptions = {
+    presets: [
+      "es2015",
+      "react"
+    ],
+    plugins: [
+      "transform-react-jsx"
+    ]
+  }
+
+
+  var jsList = []
+
+  /* Files sorting and babel */
+  fs.readdirSync(sourcePath).forEach(file => {
+    if (file.endsWith('.js')){
+      if (!fs.existsSync(tempPath)) {
+        console.log('[babel conversion] making temporary directory...');
+        fs.mkdirSync(tempPath);
+        console.log('[babel conversion] making temporary directory...done');
+
+      }
+      console.log('[babel conversion] js file conversion...');
+      var filePath = path.join(sourcePath, file);
+      var newfilePath = path.join(tempPath, file);
+      var newCode = babel.transformFileSync(filePath, babelOptions).code;
+      jsList.push(path.join(tempPath,file));
+      fs.writeFileSync(newfilePath, newCode);
+      console.log('[babel conversion] js file conversion...done');
+    } else if (file==="node_modules"){
+      //just in case
+    } else {
+      if (!fs.existsSync(destPath)) {
+        console.log('[plugin directory] making plugin directory...');
+        fs.mkdirSync(destPath);
+        console.log('[plugin directory] making plugin directory...done');
+      }
+      console.log('[plugin directory] copying source file to plugin...');
+      fs.copyFileSync(path.join(sourcePath, file), path.join(destPath, file))
+      console.log('[plugin directory] copying source file to plugin...done');
+    }
+  });
+
+  console.log('[bundling] creating bundle...');
+  /* Bundling with browserify */
+  var b = browserify(jsList, {
+    standalone: "Main"
+  });
+  console.log('[bundling] writing bundle...');
+  console.log(path.join(destPath, 'plugin.js'));
+  var bundlePath = fs.createWriteStream(path.join(destPath, 'plugin.js'));
+  b.bundle().pipe(bundlePath);
+
+  /* Remove temp file */
+  console.log('[clearing up]');
+
+  bundlePath.on('finish', function(){
+    console.log('[clearing up] deleting temporary files...');
+    for (var i = 0; i < jsList.length; i++) {
+      fs.unlinkSync(jsList[i])
+    }
+    console.log('[clearing up] deleting temporary files...done');
+
+    fs.rmdirSync(tempPath, (err => {
+      console.log(err);
+    }))
+  })
+}
+
+console.log('Welcome to the Skripto Plugin Builder');
+program
+  .version('0.0.1')
+  .command('build <src> <output>')
+  .description('Description.')
+  .option('-s, --src [src]', 'Source code directory; default is src/', 'src/')
+  .option('-o, --output [output]', 'Plugin directory; default is plugin/', 'plugin/')
+  .action(buildAction)
+
+program.parse(process.argv)
+
+/* Config */
+
+
+
+/*for (var i = 0; i < jsList.length; i++) {
+  fs.unlinkSync(jsList[i])
+}
+fs.rmdirSync(tempPath, (err => {
+  console.log(err);
+}))
+*/
